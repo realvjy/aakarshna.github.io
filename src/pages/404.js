d549d233be937b832bc2839033ba2245002b4cad
vijay verma <vijay@zomato.com>
2020-01-19 22:42:39 +0530

    404 fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,8 +14,10 @@ class NotFoundPage extends React.Component {
         <SEO title="404: Not Found" />
         <section>
           <div className="container">
-            <h1>Not Found</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <div className="error">
+              <h1>Sorry this page isn't available.</h1>
+              <p>The link you followed may be broken, or the page may have been removed. <a href="/">Go Back to homepage.</a></p>
+            </div>
           </div>
         </section>
       </Layout>
