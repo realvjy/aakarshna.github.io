37a1f23e4e7e6218dbffbaaec72b4c78cc68c750
vijay verma <realvjy@gmail.com>
2020-02-09 18:11:37 +0530

    fix typo

--- src/pages/404.js
+++ src/pages/404.js
@@ -16,7 +16,7 @@ class NotFoundPage extends React.Component {
           <div className="container">
             <div className="error">
               <h1>Sorry this page isn't available.</h1>
-              <p>The link you followed may be broken, or the page may have been removed. <a href="/">Go Back to homepage.</a></p>
+              <p>The link you followed may be broken, or the page may have been removed. <a href="/">Go back to homepage.</a></p>
             </div>
           </div>
         </section>
